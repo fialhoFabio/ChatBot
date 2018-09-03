f981e58b41c2fd2ab84ecfa3bc55f44245cf8713
fialhoFabio <fabiom456@gmail.com>
2018-09-02 23:09:32 -0300

    ChatBotController

---
+++ public/ChatBotController.js
@@ -0,0 +1,31 @@
+/*  eslint no-underscore-dangle: ["error", { "allowAfterThis": true }]  */
+
+// its used on script.js
+class ChatBotController {
+  constructor() {
+    this.conversation_id = {};
+    this.uri = 'http://localhost:3000/conversation/';
+  }
+
+  _requestWatsonResponse(text) {
+    return fetch(this.uri, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        context: this.conversation_id,
+        text,
+      }),
+    });
+  }
+
+  async _resolveWatsonResponseData(value) {
+    const response = await this._requestWatsonResponse(value);
+    return response.json();
+  }
+
+  async sendMessage(userMessage = '', cb) {
+    const data = await this._resolveWatsonResponseData(userMessage);
+    this.conversation_id = data.context;
+    cb(data.output.text);
+  }
+}
